9aa748c1ecc00c00de3fb8ea1184c94ba5cbcf30
amahabal <amahabal@19b06628-3642-0410-8fdd-33ae7300e995>
2006-03-18 14:49:05 +0000

    added codefamily SetLiteralCat, and tested it too
    git-svn-id: http://seqsee1.googlecode.com/svn/Seqsee/branches/summer05@270 19b06628-3642-0410-8fdd-33ae7300e995

---
+++ lib/SCat/reln_based.pm
@@ -0,0 +1,62 @@
+#####################################################
+#
+#    Package: SCat::reln_based
+#
+#####################################################
+#####################################################
+
+package SCat::reln_based;
+use strict;
+use Carp;
+use Class::Std;
+use base qw{};
+use Class::Multimethods;
+use Smart::Comments;
+
+multimethod 'are_relns_compatible';
+
+my $builder = sub {
+    my ( $self ) = @_;
+    confess "Should never be called!";
+};
+
+my $instancer = sub {
+    my ( $self, $object ) = @_;
+    my $reln = $object->get_underlying_reln;
+    ## underlying_reln: $reln
+    return unless $reln;
+
+    my @parts = map { $_->get_effective_object } @{ $object->get_parts_ref };
+    my $parts_count = scalar(@parts);
+
+    for (my $i=0; $i< $parts_count - 1;$i++) {
+        my $rel_between_parts = $parts[$i]->get_relation($parts[$i+1]);
+        ## $rel_between_parts, $parts[$i], $parts[$i+1]
+        return unless $rel_between_parts;
+        return unless are_relns_compatible($reln, $rel_between_parts);
+    }
+    ## instancer accepted!
+
+    my $slippages = {};
+    @parts = @{ $object->get_parts_ref };
+    for (my $i=0; $i< $parts_count;$i++) {
+        my $part = $parts[$i];
+        if ($part->get_metonym_activeness) {
+            my $metonym = $part->get_metonym;
+            $slippages->{$i} = $metonym;
+            ## $slippages->{$i}
+        }
+    }    
+
+    return SBindings->create( $slippages, { relation => $reln }, $object);
+};
+
+our $reln_based =
+    SCat::OfObj->new({
+        name => 'reln_based',
+        builder => $builder,
+        instancer => $instancer,
+    });
+
+1; 
+
