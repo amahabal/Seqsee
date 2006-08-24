cf4b39ec1694666e2dae0a47ead149377c2aaf62
amahabal <amahabal@19b06628-3642-0410-8fdd-33ae7300e995>
2006-08-24 20:04:40 +0000

    SHistory.pm added
    git-svn-id: http://seqsee1.googlecode.com/svn/Seqsee/branches/summer05@309 19b06628-3642-0410-8fdd-33ae7300e995

---
+++ lib/SHistory.pm
@@ -0,0 +1,33 @@
+#####################################################
+#
+#    Package: SHistory
+#
+#####################################################
+#####################################################
+
+package SHistory;
+use strict;
+use Carp;
+use Class::Std;
+use base qw{};
+
+my %messages_of :ATTR( :get<history>);
+
+sub BUILD{
+    my ( $self, $id, $opts ) = @_;
+    $messages_of{$id} = [ history_string("created") ];
+}
+
+sub history_string{
+    my ( $msg ) = @_;
+    return "[$::Steps_Finished]$::CurrentRunnableString\t$msg";
+}
+
+sub add_history{
+    my ( $self, $msg ) = @_;
+    push @{$messages_of{ident $self}}, history_string($msg);
+}
+
+1; 
+
+
