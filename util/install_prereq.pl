d5b8cbdcb65c9acbde272038028e3eb33a632f43
amahabal <amahabal@19b06628-3642-0410-8fdd-33ae7300e995>
2006-09-20 01:46:08 +0000

    install_prereq.pl added
    git-svn-id: http://seqsee1.googlecode.com/svn/Seqsee/branches/summer05@348 19b06628-3642-0410-8fdd-33ae7300e995

---
+++ util/install_prereq.pl
@@ -0,0 +1,45 @@
+use CPAN;
+
+my @reqs = qw{
+Class::Multimethods
+Class::Std
+Config::Std
+Devel::StackTrace
+Digest::MD5
+Exception::Class
+File::Basename
+File::Glob
+File::Spec
+File::Spec::Unix
+File::Spec::Win32
+Filter::Simple
+Filter::Util::Call
+Getopt::Long
+List::Util
+Log::Log4perl
+Memoize
+POSIX
+Perl6::Export
+Perl6::Form
+Scalar::Util
+Smart::Comments
+Sub::Installer
+Sub::Uplevel
+Sys::Hostname
+Test::Builder
+Test::Deep
+Test::Exception
+Test::More
+Test::Stochastic
+Text::Balanced
+Time::HiRes
+UNIVERSAL::require
+XSLoader
+enum
+version
+};
+
+for my $mod (@reqs) {
+    my $obj = CPAN::Shell->expand('Module',$mod);
+    $obj->install;
+}
