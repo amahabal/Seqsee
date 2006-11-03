687b22aeef195b29d79dd317fc120b61f6e3d83a
amahabal <amahabal@19b06628-3642-0410-8fdd-33ae7300e995>
2006-11-03 21:49:00 +0000

    Biblio now works (thanks to Lars Madsen's help).
    git-svn-id: http://seqsee1.googlecode.com/svn/Seqsee/branches/summer05@404 19b06628-3642-0410-8fdd-33ae7300e995

--- lib/SPos.pm
+++ lib/SPos.pm
@@ -14,16 +14,18 @@ use SPos::Backward;
 
 {
     my %MEMO;
+
     sub new {
-        my ($package, $what, $type) = @_;
+        my ( $package, $what, $type ) = @_;
         confess unless $what =~ m/^ -? \d+ $/ox;
         unless ($type) {
-            $type = ($what > 0) ? "Forward" :
-                ($what < 0) ? "Backward" :
-                    confess "SPos->new(0) illegal";
+            $type =
+                  ( $what > 0 ) ? "Forward"
+                : ( $what < 0 ) ? "Backward"
+                :                 confess "SPos->new(0) illegal";
         }
         my $key = "$what;$type";
-        return $MEMO{$key} ||= "SPos::$type"->new({index => $what});
+        return $MEMO{$key} ||= "SPos::$type"->new( { index => $what } );
     }
 }
 
