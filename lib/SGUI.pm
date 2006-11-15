256345043067823a38f86b9c14baf5c667b2cada
amahabal <amahabal@19b06628-3642-0410-8fdd-33ae7300e995>
2006-11-15 14:43:59 +0000

    Ha! Now the GUI is configured from a config file, down to bindings and buttons :)
    STill need to enable choosing file from commandline, and also make GUI_ws.conf changeable...
    
    git-svn-id: http://seqsee1.googlecode.com/svn/Seqsee/branches/summer05@425 19b06628-3642-0410-8fdd-33ae7300e995

--- lib/SGUI.pm
+++ lib/SGUI.pm
@@ -1,11 +1,15 @@
 package SGUI;
 use strict;
+use warnings;
+use Carp;
 use Config::Std;
+use English qw(-no_match_vars);
 use Tk::SCoderack;
 use Tk::SStream;
 use Tk::SComponents;
 use Tk::SInfo;
 use Tk::SWorkspace;
+use Smart::Comments;
 
 our $MW;
 our $Coderack;
@@ -15,37 +19,10 @@ our $Workspace;
 our $Info;
 
 sub setup {
-    read_config 'config/GUI.conf' => my %config;
-
-    $MW = new MainWindow;
-    setup_bindings($MW);
-
-    my $button_frame = $MW->Frame()->pack( -side => 'top' );
-    my $main_frame   = $MW->Frame()->pack( -side => 'top' );
-    my $bottom_frame = $MW->Frame()->pack( -side => 'top' );
-    my $tmp_frame;
-    setup_buttons($button_frame);
-
-    $tmp_frame = $main_frame->Frame()->pack( -side => 'left' );
-    $config{coderack}{-tags_provided} = tags_to_aref( $config{coderack_tags} );
-    $tmp_frame->Label( %{ $config{labels} }, -text => "Coderack" )->pack( -side => 'top' );
-    $Coderack = $tmp_frame->SCoderack( %{ $config{coderack} } )->pack( -side => "top" );
-
-    $tmp_frame = $main_frame->Frame()->pack( -side => 'left' );
-    $config{stream}{-tags_provided} = tags_to_aref( $config{stream_tags} );
-    $tmp_frame->Label( %{ $config{labels} }, -text => "Stream" )->pack( -side => 'top' );
-    $Stream = $tmp_frame->SStream( %{ $config{stream} } )->pack( -side => "top" );
-
-    $tmp_frame = $main_frame->Frame()->pack( -side => 'left' );
-    $config{components}{-tags_provided} = tags_to_aref( $config{components_tags} );
-    $tmp_frame->Label( %{ $config{labels} }, -text => "Components" )->pack( -side => 'top' );
-    $Components = $tmp_frame->SComponents( %{ $config{components} } )->pack( -side => "top" );
-
-    $config{info}{-tags_provided} = tags_to_aref( $config{info_tags} );
-    $Info = $bottom_frame->SInfo( %{ $config{info} } )->pack( -side => "left" );
-    $Workspace = $bottom_frame->SWorkspace( %{ $config{workspace} } )
-        ->pack( -side => "left", -fill => "x" );
-
+    read_config 'config/GUI_classic.conf' => my %config;
+    CreateWidgets( \%config );
+    SetupButtons( \%config );
+    SetupBindings( \%config );
 }
 
 sub Update {
@@ -72,76 +49,6 @@ sub tags_to_aref {
     return \@ret;
 }
 
-sub setup_buttons {
-    my ($frame) = @_;
-    my $button;
-
-    my @names_cmds = (
-        [   step => sub {
-                main::Interaction_step();
-                }
-        ],
-        [   'step 5' => sub {
-                Seqsee::Interaction_step_n(
-                    {   n            => 5,
-                        update_after => 5,
-                        max_steps    => $Global::Options_ref->{max_steps},
-                    }
-                );
-                }
-        ],
-        [   continue => sub {
-                main::Interaction_continue();
-                }
-        ],
-    );
-    for (@names_cmds) {
-        my ( $name, $cmd ) = @$_;
-        $button = $frame->Button(
-            -text    => $name,
-            -command => $cmd,
-        )->pack( -side => 'left' );
-    }
-
-}
-
-sub setup_bindings {
-    my ($mw) = @_;
-    $mw->bind(
-        '<KeyPress-s>' => sub {
-            main::Interaction_step();
-        }
-    );
-    $mw->bind(
-        '<KeyPress-c>' => sub {
-            main::Interaction_continue();
-        }
-    );
-    for my $N ( 1 .. 9 ) {
-        $mw->bind(
-            "<KeyPress-$N>" => sub {
-                Seqsee::Interaction_step_n(
-                    {   n            => 5 * $N,
-                        update_after => 5 * $N,
-                        max_steps    => $Global::Options_ref->{max_steps},
-                    }
-                );
-            }
-        );
-    }
-    $mw->bind(
-        '<KeyPress-q>' => sub {
-            exit;
-        }
-    );
-    $mw->bind(
-        '<KeyPress-p>' => sub {
-            $Global::Break_Loop = 1;
-        }
-    );
-
-}
-
 sub ask_seq {
     my $top = $MW->Toplevel( -title => "Seqsee Sequence Entry" );
     $top->Label( -text => "Enter sequence(space separated): " )->pack( -side => 'left' );
@@ -162,6 +69,80 @@ sub ask_seq {
         }
     );
 
+}
+
+sub CreateWidgets {
+    my ($config_ref) = @_;
+    $MW = new MainWindow();
+
+    my $frames_string = $config_ref->{frames}{frames} or confess;
+    my @lines = split qq{\n}, $frames_string;
+    for my $line (@lines) {
+        $line =~ s#^\s*##;
+        $line =~ s#\s*$##;
+        my ( $name, $parent, $widget_type, $position, @rest ) = split( /\s+/, $line );
+        ## In CreateWidgets: $name, $parent, $widget_type, $position, @rest
+        no strict;
+        my $widget
+            = ${$parent}->$widget_type( GetWidgetOptions( $widget_type, $config_ref, @rest ) )
+            ->pack( -side => $position );
+        ${$name} = $widget unless $name eq '_';
+    }
+}
+
+sub SetupButtons {
+    my ($config_ref) = @_;
+
+    my $parent_name = $config_ref->{frames}{buttons_widget} or confess;
+    my $parent;
+    { no strict; $parent = ${$parent_name}; }
+
+    my $button_order = $config_ref->{frames}{button_order} or confess;
+    my @buttons_names = map { s#^\s*##; s#\s*$##; s#\s+# #g; $_ } split( qq{\n}, $button_order );
+
+    my $options_ref = $config_ref->{Button};
+    my %options = ( defined $options_ref ) ? %$options_ref : ();
+
+    for (@buttons_names) {
+        my $command_string = $config_ref->{buttons}{$_} or confess;
+        my $command = eval qq{ sub {$command_string}; };
+        confess if $EVAL_ERROR;
+        $parent->Button( -text => $_, -command => $command, %options )->pack( -side => 'left' );
+    }
+}
+
+sub SetupBindings {
+    my ($config_ref) = @_;
+    my @names = keys %{ $config_ref->{bindings} };
+    for my $name (@names) {
+        ## $name: $name
+        my $command_string = $config_ref->{bindings}{$name} or confess;
+        my $command = eval qq{ sub {$command_string}; };
+        confess if $EVAL_ERROR;
+        $MW->bind( $name => $command );
+    }
+}
+
+{
+    my %SeqseeWidgets = map { $_ => 1 } qw(SCoderack SStream SComponents SInfo SWorkspace);
+
+    sub GetWidgetOptions {
+        my ( $type, $config_ref, @rest ) = @_;
+        if ( exists $SeqseeWidgets{$type} ) {
+            my %ret = %{ $config_ref->{$type} } or confess;
+            my $tags_config = $config_ref->{ $type . '_tags' };
+            if ( defined $tags_config ) {
+                $ret{'-tags_provided'} = tags_to_aref($tags_config);
+            }
+            return ( %ret, @rest );
+
+        }
+        else {
+            my $extra_config = $config_ref->{$type};
+            my %extra_config = ( defined $extra_config ) ? %$extra_config : ();
+            return ( %extra_config, @rest );
+        }
+    }
 }
 
 1;
