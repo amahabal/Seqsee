d061d4155d9e3ea76c92fddec97120cd5ab841b1
amahabal <amahabal@19b06628-3642-0410-8fdd-33ae7300e995>
2007-06-02 05:21:39 +0000

    keys 1..n work now to repsond to commentary
    git-svn-id: http://seqsee1.googlecode.com/svn/Seqsee/branches/summer05@574 19b06628-3642-0410-8fdd-33ae7300e995

--- lib/Tk/SCommentary.pm
+++ lib/Tk/SCommentary.pm
@@ -12,65 +12,81 @@ use base qw/Tk::Derived Tk::Frame/;
 my $Text;
 my $ButtonFrame;
 my @Buttons;
+my $active_button_count;
 
-my $Response; # Button pressed. When message displayed requiring action,
-              # this is the variable waited on.
+my $Response;    # Button pressed. When message displayed requiring action,
+                 # this is the variable waited on.
 
 Construct Tk::Widget 'SCommentary';
-sub Update{    
+
+sub Update {
 }
 
-sub Populate{
+sub Populate {
     my ( $self, $args ) = @_;
-    $Text = $self->Scrolled('ROText', -scrollbars => 'se',  %$args )->pack(-side => 'left');
-    $Text->bind('<KeyPress>', sub {Tk->break()});
-    $Text->bind('<KeyPress-q>', sub {exit});
-    $ButtonFrame = $self->Frame()->pack(-side => 'right');
-    for my $button_number (0..3) {
+    $Text =
+      $self->Scrolled( 'ROText', -scrollbars => 'se', %$args )
+      ->pack( -side => 'left' );
+    $Text->bind( '<KeyPress>',   sub { Tk->break() } );
+    $Text->bind( '<KeyPress-q>', sub { exit } );
+    $ButtonFrame = $self->Frame()->pack( -side => 'right' );
+    for my $button_number ( 0 .. 3 ) {
         push @Buttons,
-            $ButtonFrame->Button(-text => '',
-                                 -command => sub { $Response = $button_number },
-                                 -width => 15,
-                                 -state => 'disabled',
-                                     )->pack(-side => 'top');
+          $ButtonFrame->Button(
+            -text    => '',
+            -command => sub { $Response = $button_number },
+            -width   => 15,
+            -state   => 'disabled',
+          )->pack( -side => 'top' );
+        my $key_to_press_to_activate = $button_number + 1;
+        $Text->bind(
+            "<KeyPress-$key_to_press_to_activate>",
+            sub {
+                return unless $button_number < $active_button_count;
+                $Response = $button_number,;
+            }
+        );
     }
 }
 
 sub MessageRequiringNoResponse {
     my ( $self, @msg ) = @_;
-    $Text->insert('end', @msg);
+    $Text->insert( 'end', @msg );
     $Text->see('end');
 }
 
-sub MessageRequiringAResponse{
+sub MessageRequiringAResponse {
     my ( $self, $response_ref, @msg ) = @_;
-    $Text->insert('end', @msg);
+    $Text->insert( 'end', @msg );
     $Text->see('end');
 
     my $i = 0;
     for (@$response_ref) {
-        $Buttons[$i]->configure(-text => $_, -state => 'normal');
+        $Buttons[$i]->configure( -text => $_, -state => 'normal' );
         $i++;
     }
+    $active_button_count = $i;
+
     $Response = -1;
     $Text->focus();
     $self->grab();
-    $SGUI::MW->waitVariable(\$Response);
+    $SGUI::MW->waitVariable( \$Response );
     $self->grabRelease();
     $SGUI::Workspace->focus();
-    for (0..3) {
-        $Buttons[$_]->configure(-text => '', -state => 'disabled');
+    $active_button_count = 0;
+    for ( 0 .. 3 ) {
+        $Buttons[$_]->configure( -text => '', -state => 'disabled' );
     }
     return $response_ref->[$Response];
 }
 
-sub MessageRequiringBooleanResponse{
+sub MessageRequiringBooleanResponse {
     my ( $self, @msg ) = @_;
-    my $res = $self->MessageRequiringAResponse(['yes', 'no'], @msg);
-    #main::message("Response: *$res*");
-    return ($res eq 'yes') ? 1 : 0;
-}
+    my $res = $self->MessageRequiringAResponse( [ 'yes', 'no' ], @msg );
 
+    #main::message("Response: *$res*");
+    return ( $res eq 'yes' ) ? 1 : 0;
+}
 
 1;
 
