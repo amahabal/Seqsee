87f548f38fb171459266206794a704fd83152373
amahabal <amahabal@19b06628-3642-0410-8fdd-33ae7300e995>
2007-08-03 06:49:39 +0000

    descending.pm fixed like ascending.pm
    git-svn-id: http://seqsee1.googlecode.com/svn/Seqsee/branches/summer05@592 19b06628-3642-0410-8fdd-33ae7300e995

--- lib/SCat/descending.pm
+++ lib/SCat/descending.pm
@@ -13,8 +13,17 @@ use base qw{};
 
 my $builder = sub {
     my ( $self, $args_ref ) = @_;
-    croak q{need start} unless $args_ref->{start};
-    croak q{need end}   unless $args_ref->{end};
+    my $params_count;
+    for (qw{start end length}) {
+        $params_count++ if exists $args_ref->{$_};
+    }
+    confess 'Too few params' if $params_count < 2;
+    my ($start, $end);
+    $start = exists($args_ref->{start})?$args_ref->{start}: $args_ref->{end}+$args_ref->{length} - 1;
+    $end = exists($args_ref->{end})?$args_ref->{end}: $args_ref->{start}-$args_ref->{length} + 1;
+    $args_ref->{start} ||= $start; 
+    $args_ref->{end} ||= $end;
+    $args_ref->{length} ||= $start -$end + 1;
 
     my $ret = SObject->create( reverse( $args_ref->{end} .. $args_ref->{start} ) );
     $ret->add_category( $self, SBindings->create( {}, $args_ref, $ret ) );
@@ -32,6 +41,12 @@ our $descending = SCat::OfObj->new(
         positions => {
             start => SPos->new(1),
             end   => SPos->new(-1),
+        },
+        sufficient_atts => {
+            'end:start' => 1,
+            'length:start' => 1,
+            'end:length' => 1,
+            'end:length:start' => 1, # stupid!!
         },
     }
 );
