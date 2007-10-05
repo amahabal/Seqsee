186053875498acd4d2673ec4e04d075f1d1ff4eb
amahabal <amahabal@19b06628-3642-0410-8fdd-33ae7300e995>
2007-10-05 17:15:58 +0000

    coderack display dual column now...
    git-svn-id: http://seqsee1.googlecode.com/svn/Seqsee/branches/feature_extend_long_relations@687 19b06628-3642-0410-8fdd-33ae7300e995

--- lib/SGUI/Coderack.pm
+++ lib/SGUI/Coderack.pm
@@ -13,7 +13,7 @@ my ( $XOffset, $YOffset );
 my $Margin;
 my $EffectiveHeight;
 my $EffectiveWidth;
-my ( $MaxRows, $RowHeight );
+my ( $MaxRows, $MaxColumns, $RowHeight, $ColumnWidth );
 my ( $NameOffset, $CountOffset, $UrgencyOffset, $HistoricalFractionOffset );
 
 BEGIN {
@@ -21,8 +21,8 @@ BEGIN {
     $Margin = $config{Layout}{Margin};
 
     my %CoderackOptions = %{ $config{CoderackLayout} };
-    ( $MaxRows, $NameOffset, $CountOffset, $UrgencyOffset, $HistoricalFractionOffset )
-        = @CoderackOptions{ 'MaxRows', 'NameOffset', 'CountOffset', 'UrgencyOffset',
+    ( $MaxRows, $MaxColumns, $NameOffset, $CountOffset, $UrgencyOffset, $HistoricalFractionOffset )
+        = @CoderackOptions{ 'MaxRows', 'MaxColumns', 'NameOffset', 'CountOffset', 'UrgencyOffset',
         'HistoricalFractionOffset' };
 }
 
@@ -32,6 +32,7 @@ sub Setup {
     $EffectiveHeight = $Height - 2 * $Margin;
     $EffectiveWidth  = $Width - 2 * $Margin;
     $RowHeight       = int( $EffectiveHeight / $MaxRows );
+    $ColumnWidth     = int( $EffectiveWidth / $MaxColumns);
 }
 
 sub DrawIt {
@@ -61,18 +62,18 @@ sub DrawIt {
     }
 
     my $total_run_so_far = List::Util::sum( values %SCoderack::HistoryOfRunnable );
-    my $rows_displayed   = 0;
-    my $base_x_offset    = $XOffset + $Margin;
 
-    $Canvas->createText(
-        $base_x_offset + $NameOffset, $YOffset - 10,
-        -anchor => 'nw',
-        -text   => "NAME",
-    );
-    $Canvas->createText(
-        $base_x_offset + $CountOffset, $YOffset - 10,
-        -anchor => 'nw',
-        -text   => "CURR. COUNT",
+    for my $column_number (0..$MaxColumns-1) {
+        my $base_x_offset = $XOffset + $Margin + $column_number * $ColumnWidth;
+        $Canvas->createText(
+            $base_x_offset + $NameOffset, $YOffset - 10,
+            -anchor => 'nw',
+            -text   => "NAME",
+                );
+        $Canvas->createText(
+            $base_x_offset + $CountOffset, $YOffset - 10,
+            -anchor => 'nw',
+            -text   => "#",
     );
     $Canvas->createText(
         $base_x_offset + $UrgencyOffset, $YOffset - 10,
@@ -84,12 +85,23 @@ sub DrawIt {
         -anchor => 'nw',
         -text   => "% OF ALL RUN",
     );
+    }
 
     for (keys %count) {
         $SCoderack::HistoryOfRunnable{$_} ||= 0;
     }
+    my $current_column = 0;
+    my $rows_displayed   = 0;
+    my $base_x_offset    = $XOffset + $Margin;
+
     while ( my ( $family, $historical_count ) = each %SCoderack::HistoryOfRunnable ) {
-        last if $rows_displayed > $MaxRows;
+        if ($rows_displayed > $MaxRows) {
+            $rows_displayed = 0;
+            $current_column++;
+            $base_x_offset += $ColumnWidth;
+        }
+
+        last if $current_column > $MaxColumns;
         my $y_pos = $YOffset + $Margin + $rows_displayed * $RowHeight;
         $Canvas->createText(
             $base_x_offset + $NameOffset, $y_pos,
@@ -104,35 +116,35 @@ sub DrawIt {
         $Canvas->createRectangle(
             $base_x_offset + $UrgencyOffset,
             $y_pos,
-            $base_x_offset + $UrgencyOffset + $sum{$family},
+            $base_x_offset + $UrgencyOffset + $sum{$family} * 0.5,
             $y_pos + 0.8 * $RowHeight,
             -fill => '#0000FF',
         );
         $Canvas->createRectangle(
-            $base_x_offset + $UrgencyOffset + 99,
+            $base_x_offset + $UrgencyOffset + 49,
             $y_pos,
-            $base_x_offset + $UrgencyOffset + 100,
+            $base_x_offset + $UrgencyOffset + 50,
             $y_pos + 0.8 * $RowHeight,
         );
 
         $Canvas->createRectangle(
             $base_x_offset + $HistoricalFractionOffset,
             $y_pos,
-            $base_x_offset + $HistoricalFractionOffset + 100 * $historical_count
+            $base_x_offset + $HistoricalFractionOffset + 50 * $historical_count
                 / $total_run_so_far,
             $y_pos + 0.8 * $RowHeight,
             -fill => '#FF0000',
         ) if $total_run_so_far;
         $Canvas->createText(
-            $base_x_offset + $HistoricalFractionOffset + 120,
+            $base_x_offset + $HistoricalFractionOffset + 60,
             $y_pos,
             -text   => $historical_count,
             -anchor => 'nw'
         );
         $Canvas->createRectangle(
-            $base_x_offset + $HistoricalFractionOffset + 99,
+            $base_x_offset + $HistoricalFractionOffset + 49,
             $y_pos,
-            $base_x_offset + $HistoricalFractionOffset + 100,
+            $base_x_offset + $HistoricalFractionOffset + 50,
             $y_pos + 0.8 * $RowHeight,
         );
 
@@ -146,7 +158,7 @@ sub DrawIt {
                 my $y2 = $YOffset + ( 4 + $rows_displayed ) * $RowHeight - 3;
                 my $id = $Canvas->createRectangle(
                     $base_x_offset,                   $y,
-                    $base_x_offset + $EffectiveWidth, $y2,
+                    $base_x_offset + $ColumnWidth, $y2,
                     -fill    => '#CCFFDD',
                     -outline => '',
                 );
