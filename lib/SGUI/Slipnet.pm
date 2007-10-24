8bdbadea4a2b9382e75e64169eb9d27a4b087dd8
amahabal <amahabal@19b06628-3642-0410-8fdd-33ae7300e995>
2007-10-24 03:55:13 +0000

    Compiler/Filters/SeqseeDisplay.pm now used in one more place... Slipnet.pm
    git-svn-id: http://seqsee1.googlecode.com/svn/Seqsee/branches/feature_extend_long_relations@706 19b06628-3642-0410-8fdd-33ae7300e995

--- lib/SGUI/Slipnet.pm
+++ lib/SGUI/Slipnet.pm
@@ -1,95 +1,56 @@
-#####################################################
-#
-#    Package: SGUI::Slipnet
-#
-#####################################################
-#####################################################
+SeqseeDisplay Slipnet is {
+ConfigNames: {
+        EntriesPerColumn ColumnCount MaxOvalRadius MaxTextWidth MinActivationForDisplay
+    }
+Variables: { ColumnWidth RowHeight }
+Setup: {
+        $ColumnWidth = int( $EffectiveWidth / $ColumnCount );
+        $RowHeight   = int( $EffectiveHeight / $EntriesPerColumn );
+    }
+DrawIt: {
+        my @concepts_with_activation = SLTM::GetTopConcepts(10);
+        my ( $row, $col ) = ( -1, 0 );
+        for (@concepts_with_activation) {
+            last if $col >= $ColumnCount;
+            next unless $_->[1] > $MinActivationForDisplay;
+            $row++;
+            if ( $row >= $EntriesPerColumn ) {
+                $row = 0;
+                $col++;
+            }
+            DrawNode(
+                $_,
+                $XOffset + $Margin + $col * $ColumnWidth,
+                $YOffset + $Margin + $row * $RowHeight
+            );
 
-package SGUI::Slipnet;
-use strict;
-use Carp;
-use Class::Std;
-use Config::Std;
-use base qw{};
-
-my $Canvas;
-my ($Height, $Width);
-my ($XOffset, $YOffset);
-
-my $Margin;
-my $EffectiveHeight;
-my $EffectiveWidth;
-
-# Slipnet display related variables
-my $NetEntriesPerColumn;
-my $NetColumnCount;
-my $NetMaxOvalRadius;
-my $NetMaxTextWidth;
-my $NetColumnWidth;
-my $NetRowHeight;
-my $NetMinActivationForDisplay;
-
-BEGIN {
-    read_config 'config/GUI_ws3.conf' => my %config;
-    $Margin = $config{Layout}{Margin};
-    my $NetLayoutOptions    = $config{NetLayout};
-    $NetEntriesPerColumn = $NetLayoutOptions->{'EntriesPerColumn'};
-    $NetColumnCount      = $NetLayoutOptions->{'ColumnCount'};
-    $NetMaxOvalRadius      = $NetLayoutOptions->{'MaxOvalRadius'};
-    $NetMaxTextWidth     = $NetLayoutOptions->{'MaxTextWidth'};
-    $NetMinActivationForDisplay = $NetLayoutOptions->{'MinActivationForDisplay'};
-}
-
-sub Setup {
-    my $package = shift;
-    ( $Canvas, $XOffset, $YOffset, $Width, $Height ) = @_;
-    $EffectiveHeight = $Height - 2 * $Margin;
-    $EffectiveWidth  = $Width - 2 * $Margin;
-    $NetColumnWidth = int( $EffectiveWidth / $NetColumnCount );
-    $NetRowHeight   = int( $EffectiveHeight / $NetEntriesPerColumn );
-}
-
-sub DrawIt {
-    my @concepts_with_activation = SLTM::GetTopConcepts(10);
-    my ( $row, $col ) = ( -1, 0 );
-    for (@concepts_with_activation) {
-        last if $col >= $NetColumnCount;
-        next unless $_->[1] > $NetMinActivationForDisplay;
-        $row++;
-        if ( $row >= $NetEntriesPerColumn ) {
-            $row = 0;
-            $col++;
         }
-        NetDrawNode(
-            $_,
-            $XOffset + $Margin + $col * $NetColumnWidth,
-            $YOffset + $Margin + $row * $NetRowHeight
-        );
+    }
+ExtraStuff: {
+        sub DrawNode {
+            my ( $con_ref, $left, $top ) = @_;
+            my ( $concept, $activation, $raw_activation, $raw_significance ) = @{$con_ref};
+            my $radius = $activation * $MaxOvalRadius;
+
+            #main::message("Rad: $radius");
+            $Canvas->createOval(
+                $left + 2 + $MaxOvalRadius - $radius,
+                $top + 2 + $MaxOvalRadius - $radius,
+                $left + 2 + $MaxOvalRadius + $radius,
+                $top + 2 + $MaxOvalRadius + $radius,
+                Style::NetActivation( int($raw_significance) ),
+            );
+            my $text = $concept->as_text();
+            $text = substr( $text, 0, $MaxTextWidth );
+            $Canvas->createText(
+                $left + 6 + 2 * $MaxOvalRadius,
+                $top + 2 + $MaxOvalRadius,
+                -anchor => 'w',
+                -text   => $text,
+            );
+        }
 
     }
-}
-
-sub NetDrawNode {
-    my ( $con_ref, $left, $top ) = @_;
-    my ( $concept, $activation, $raw_activation, $raw_significance ) =
-      @{$con_ref};
-    my $radius = $activation* $NetMaxOvalRadius;
-    #main::message("Rad: $radius");
-    $Canvas->createOval(
-        $left + 2 + $NetMaxOvalRadius - $radius,
-        $top + 2 + $NetMaxOvalRadius - $radius,
-        $left + 2 + $NetMaxOvalRadius + $radius,
-        $top + 2 + $NetMaxOvalRadius + $radius,
-        Style::NetActivation(int($raw_significance)),
-    );
-    my $text = $concept->as_text();
-    $text = substr($text, 0, $NetMaxTextWidth);
-    $Canvas->createText(
-        $left + 6 + 2 * $NetMaxOvalRadius,
-        $top + 2 + $NetMaxOvalRadius,
-        -anchor => 'w',
-        -text => $text,
-    );
 }
 
 1;
