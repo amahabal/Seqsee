50eb6cb1d1cdbc65e8b4b6c5b772a8d6e60f3f83
amahabal <amahabal@19b06628-3642-0410-8fdd-33ae7300e995>
2007-12-05 22:53:29 +0000

    Refactoring continues...
    git-svn-id: http://seqsee1.googlecode.com/svn/Seqsee/branches/feature_extend_long_relations@777 19b06628-3642-0410-8fdd-33ae7300e995

--- lib/Seqsee.pm
+++ lib/Seqsee.pm
@@ -135,17 +135,6 @@ sub Seqsee_Step {
         if ( UNIVERSAL::isa( $err, 'SErr::ProgOver' ) ) {
             return 1;
         }
-        if (   UNIVERSAL::isa( $err, 'SErr::NeedMoreData' )
-            or UNIVERSAL::isa( $err, 'SErr::ContinueWith' ) )
-        {
-            if ( $err->forced() ) {
-                $err->payload()->force_to_be_next_runnable();
-            }
-            else {
-                $err->payload()->schedule();
-            }
-            return;
-        }
 
         # main::message("About to call default_error_handler with '$err'");
         # confess "Empty error message" if $err eq q{};
