77b898d32ebf92ac1a6e78fc8d7558f12ea2f17a
amahabal <amahabal@19b06628-3642-0410-8fdd-33ae7300e995>
2008-05-05 20:52:16 +0000

    Doug suggested changes to CreateRelationImage.pl: boxing the objects; different arrows etc.
    git-svn-id: http://seqsee1.googlecode.com/svn/trunk@971 19b06628-3642-0410-8fdd-33ae7300e995

--- util/CreateRelationImage.pl
+++ util/CreateRelationImage.pl
@@ -2,19 +2,36 @@ use 5.10.0;
 use Tk;
 use Smart::Comments;
 
-use constant { WIDTH => 500, HEIGHT => 100, PAGEHEIGHT => '3c' };
+use constant { WIDTH => 500, HEIGHT => 120, PAGEHEIGHT => '3c' };
 use constant {
-    ARROW_LEFT           => 100,
-    ARROW_33             => 200,
-    ARROW_66             => 300,
-    ARROW_RIGHT          => 400,
+    ARROW_LEFT           => 90,
+    ARROW_33             => 90,
+    ARROW_66             => 400,
+    ARROW_RIGHT          => 410,
     ARROW_LABEL_Y_OFFSET => 3,
     RIGHT_END_OF_FIRST   => 80,
     LEFT_END_OF_SECOND   => 420,
-        RIGHT_END_OF_LABEL => 380,
+    RIGHT_END_OF_LABEL   => 380,
+
+    Y_OFFSET => 15,
+
 };
-my ( $CATEGORY, $DESCRIPTORS_COUNT, $FIRST_OBJECT_DESCRIPTION_TB, %FIRST_OBJECT_DESCRIPTION,
-    $SECOND_OBJECT_DESCRIPTION_TB, %SECOND_OBJECT_DESCRIPTION, $CANVAS, );
+
+use constant {
+    RIGHT_END_OF_BOX1 => RIGHT_END_OF_FIRST + 20,
+    LEFT_END_OF_BOX1  => 20,
+    LEFT_END_OF_BOX2  => LEFT_END_OF_SECOND - 20,
+    RIGHT_END_OF_BOX2 => WIDTH - 20,
+    TOP_OF_BOXES      => 10 + Y_OFFSET,
+    BOTTOM_OF_BOXES   => HEIGHT - 10,
+    EFFECTIVE_HEIGHT  => HEIGHT - Y_OFFSET
+};
+my (
+    $CATEGORY,                     $DESCRIPTORS_COUNT,
+    $FIRST_OBJECT_DESCRIPTION_TB,  %FIRST_OBJECT_DESCRIPTION,
+    $SECOND_OBJECT_DESCRIPTION_TB, %SECOND_OBJECT_DESCRIPTION,
+    $CANVAS,
+);
 
 my $DIR = 'D:/DISSERTATION/Chapters/RelationEPS';
 my $FILENAME;
@@ -25,12 +42,8 @@ $MW->bind(
         exit;
     }
 );
-$MW->bind(
-    '<F1>' => \&Draw,
-);
-$MW->bind(
-    '<F10>' => \&Save,
-);
+$MW->bind( '<F1>'  => \&Draw, );
+$MW->bind( '<F10>' => \&Save, );
 $MW->focusmodel('active');
 
 #====
@@ -47,34 +60,38 @@ $button->focus();
 {
     my $f = $MW->Frame()->pack( -side => 'top' );
     $f->Label( -text => 'Descriptors' )->pack( -side => 'left' );
-    $f->Entry( -textvariable => \$DESCRIPTORS, -width => 100 )->pack( -side => 'left' );
+    $f->Entry( -textvariable => \$DESCRIPTORS, -width => 100 )
+      ->pack( -side => 'left' );
 }
 {
     my $f = $MW->Frame()->pack( -side => 'top' );
     $f->Label( -text => 'First' )->pack( -side => 'left' );
-    $FIRST_OBJECT_DESCRIPTION_TB
-        = $f->Text( -height => 10, -width => 100 )->pack( -side => 'left' );
+    $FIRST_OBJECT_DESCRIPTION_TB =
+      $f->Text( -height => 10, -width => 100 )->pack( -side => 'left' );
 }
 
 {
     my $f = $MW->Frame()->pack( -side => 'top' );
     $f->Label( -text => 'Second' )->pack( -side => 'left' );
-    $SECOND_OBJECT_DESCRIPTION_TB
-        = $f->Text( -height => 10, -width => 100 )->pack( -side => 'left' );
+    $SECOND_OBJECT_DESCRIPTION_TB =
+      $f->Text( -height => 10, -width => 100 )->pack( -side => 'left' );
 }
 
-$CANVAS = $MW->Canvas( -height => HEIGHT(), -width => WIDTH(), -background => 'white' )
-    ->pack( -side => 'top' );
+$CANVAS =
+  $MW->Canvas( -height => HEIGHT(), -width => WIDTH(), -background => 'white' )
+  ->pack( -side => 'top' );
 
 {
     my $f = $MW->Frame()->pack( -side => 'top' );
     $f->Label( -text => 'Filename' )->pack( -side => 'left' );
-    $f->Entry( -textvariable => \$FILENAME, -width => 100 )->pack( -side => 'left' );
+    $f->Entry( -textvariable => \$FILENAME, -width => 100 )
+      ->pack( -side => 'left' );
 }
 $DESCRIPTORS = "start, end, length";
 $CATEGORY    = "Ascending";
-$FIRST_OBJECT_DESCRIPTION_TB->insert( 'end',  "start: 6\nend: 8\n length : 3" );
-$SECOND_OBJECT_DESCRIPTION_TB->insert( 'end', "start:9:end:successor\nlength:7:end:pred" );
+$FIRST_OBJECT_DESCRIPTION_TB->insert( 'end', "start: 6\nend: 8\n length : 3" );
+$SECOND_OBJECT_DESCRIPTION_TB->insert( 'end',
+    "start:9:end:successor\nlength:7:end:pred" );
 
 MainLoop();
 
@@ -89,6 +106,7 @@ sub trim {
 
 sub Draw {
     $CANVAS->delete('all');
+    DrawBoxes();
     my @descriptors = split( ",", $DESCRIPTORS );
     trim(@descriptors);
 
@@ -100,11 +118,12 @@ sub Draw {
         $descriptor_to_index{$_} = $count++;
     }
 
-    my %firstobj_descriptions = ParseFirstObject( $FIRST_OBJECT_DESCRIPTION_TB->get( '0.0', 'end' ),
+    my %firstobj_descriptions =
+      ParseFirstObject( $FIRST_OBJECT_DESCRIPTION_TB->get( '0.0', 'end' ),
         \%descriptor_to_index );
     ### firstobj_descriptions: %firstobj_descriptions
-    my %secondobj_descriptions
-        = ParseSecondObject( $SECOND_OBJECT_DESCRIPTION_TB->get( '0.0', 'end' ),
+    my %secondobj_descriptions =
+      ParseSecondObject( $SECOND_OBJECT_DESCRIPTION_TB->get( '0.0', 'end' ),
         \%descriptor_to_index );
     ### secondobj_descriptions: %secondobj_descriptions
 
@@ -114,22 +133,23 @@ sub Draw {
         my $y_coordinate = index_to_y_coordinate($index);
         if ( $descriptor ~~ %firstobj_descriptions ) {
             $CANVAS->createText(
-                 RIGHT_END_OF_FIRST(), $y_coordinate,
-                -text   => "$descriptor=$firstobj_descriptions{$descriptor}",
+                RIGHT_END_OF_FIRST(), $y_coordinate,
+                -text   => "$descriptor = $firstobj_descriptions{$descriptor}",
                 -anchor => 'e',
             );
         }
         if ( $descriptor ~~ %secondobj_descriptions ) {
             $CANVAS->createText(
-                 LEFT_END_OF_SECOND(), $y_coordinate,
-                -text   => "$descriptor=$secondobj_descriptions{$descriptor}",
+                LEFT_END_OF_SECOND(), $y_coordinate,
+                -text   => "$descriptor = $secondobj_descriptions{$descriptor}",
                 -anchor => 'w',
             );
         }
     }
 
-    my @arrows
-        = ParseArrows( $SECOND_OBJECT_DESCRIPTION_TB->get( '0.0', 'end' ), \%descriptor_to_index );
+    my @arrows =
+      ParseArrows( $SECOND_OBJECT_DESCRIPTION_TB->get( '0.0', 'end' ),
+        \%descriptor_to_index );
     for (@arrows) {
         DrawArrow(@$_);
     }
@@ -142,10 +162,15 @@ sub DrawArrow {
     ### draw_arr: @_
     my $y1 = index_to_y_coordinate($from_index);
     my $y2 = index_to_y_coordinate($to_index);
-    $CANVAS->createLine( ARROW_LEFT(), $y1, ARROW_33(), $y1, ARROW_66(), $y2, ARROW_RIGHT(), $y2, -arrow => 'last' );
-    $CANVAS->createText( RIGHT_END_OF_LABEL(), $y2 - ARROW_LABEL_Y_OFFSET(),
-                         -text => $relation, -anchor => 'se',
-                             );
+    $CANVAS->createLine(
+        ARROW_LEFT(), $y1, ARROW_33(), $y1, ARROW_66(), $y2,
+        ARROW_RIGHT(), $y2, -arrow => 'last'
+    );
+    $CANVAS->createText(
+        RIGHT_END_OF_LABEL(), $y2 - ARROW_LABEL_Y_OFFSET(),
+        -text   => $relation,
+        -anchor => 'se',
+    );
 }
 
 sub ParseFirstObject {
@@ -158,7 +183,7 @@ sub ParseFirstObject {
         my ( $descriptor, $value ) = split( /:/, $_, 2 );
         trim( $descriptor, $value );
         say "Descriptor $descriptor non-existant!"
-            unless exists $descriptor_to_index_ref->{$descriptor};
+          unless exists $descriptor_to_index_ref->{$descriptor};
         $description{$descriptor} = $value;
     }
     return %description;
@@ -170,10 +195,11 @@ sub ParseSecondObject {
     trim(@lines);
     my %description;
     for (@lines) {
-        my ( $descriptor, $value, $from_descriptor, $relation ) = split( /:/, $_, 4 );
+        my ( $descriptor, $value, $from_descriptor, $relation ) =
+          split( /:/, $_, 4 );
         trim( $descriptor, $value );
         say "Descriptor $descriptor non-existant!"
-            unless exists $descriptor_to_index_ref->{$descriptor};
+          unless exists $descriptor_to_index_ref->{$descriptor};
         $description{$descriptor} = $value;
     }
     return %description;
@@ -185,42 +211,75 @@ sub ParseArrows {
     trim(@lines);
     my @arrows;
     for (@lines) {
-        my ( $descriptor, $value, $from_descriptor, $relation ) = split( /:/, $_, 4 );
+        my ( $descriptor, $value, $from_descriptor, $relation ) =
+          split( /:/, $_, 4 );
         $from_descriptor || next;
         trim( $descriptor, $value, $from_descriptor, $relation );
         say "Descriptor $descriptor non-existant!"
-            unless exists $descriptor_to_index_ref->{$descriptor};
+          unless exists $descriptor_to_index_ref->{$descriptor};
         say "Descriptor $from_descriptor non-existant!"
-            unless exists $descriptor_to_index_ref->{$from_descriptor};
+          unless exists $descriptor_to_index_ref->{$from_descriptor};
         push @arrows,
-            [
-            $descriptor_to_index_ref->{$from_descriptor}, $descriptor_to_index_ref->{$descriptor},
+          [
+            $descriptor_to_index_ref->{$from_descriptor},
+            $descriptor_to_index_ref->{$descriptor},
             $relation
-            ];
+          ];
     }
     return @arrows;
 }
 
 sub index_to_y_coordinate {
     my ($index) = @_;
-    return ( $index + 0.5 ) * HEIGHT() / $DESCRIPTORS_COUNT;
+    return Y_OFFSET + ( $index + 0.5 ) * EFFECTIVE_HEIGHT / $DESCRIPTORS_COUNT;
 }
 
 sub Save {
     my $filename = $FILENAME;
-    if (-e $filename) {
-        say "File exists, perhaps you already saved this? \n ***NOT SAVING AGAIN***";
+    if ( -e $filename ) {
+        say
+"File exists, perhaps you already saved this? \n ***NOT SAVING AGAIN***";
         return;
     }
-    $CANVAS->postscript( -file => $filename, -pageheight => PAGEHEIGHT, -height => HEIGHT );
+    $CANVAS->postscript(
+        -file       => $filename,
+        -pageheight => PAGEHEIGHT,
+        -height     => HEIGHT
+    );
+
     # or confess "Failed to save $filename";
     say "Saved file $filename";
 }
 
 sub GenerateFilename {
     my $count = 1;
-    while (-e "$DIR/$count.eps") {
+    while ( -e "$DIR/$count.eps" ) {
         $count++;
     }
     $FILENAME = "$DIR/$count.eps";
+}
+
+sub DrawBoxes {
+    $CANVAS->createRectangle( LEFT_END_OF_BOX1, TOP_OF_BOXES,
+        RIGHT_END_OF_BOX1, BOTTOM_OF_BOXES,
+        -fill    => '#DDDDDD',
+        -outline => '#BBBBBB',
+    );
+    $CANVAS->createRectangle( LEFT_END_OF_BOX2, TOP_OF_BOXES,
+        RIGHT_END_OF_BOX2, BOTTOM_OF_BOXES,
+        -fill    => '#DDDDDD',
+        -outline => '#BBBBBB',
+    );
+    $CANVAS->createText(
+        ( LEFT_END_OF_BOX1 + RIGHT_END_OF_BOX1 ) / 2,
+        TOP_OF_BOXES - 5,
+        -anchor => 's',
+        -text   => 'First object',
+    );
+    $CANVAS->createText(
+        ( LEFT_END_OF_BOX2 + RIGHT_END_OF_BOX2 ) / 2,
+        TOP_OF_BOXES - 5,
+        -anchor => 's',
+        -text   => 'Second object',
+    );
 }
