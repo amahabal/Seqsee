95b325ee8b70bb9cb3fb25a2e19d0ae0a541cdef
amahabal <amahabal@19b06628-3642-0410-8fdd-33ae7300e995>
2008-11-14 21:54:36 +0000

    Much more functional GenerateStats.pl
    git-svn-id: http://seqsee1.googlecode.com/svn/trunk@1009 19b06628-3642-0410-8fdd-33ae7300e995

--- performance/GenerateStats.pl
+++ performance/GenerateStats.pl
@@ -11,10 +11,19 @@ use Getopt::Long;
 use Storable;
 use File::Slurp;
 use Smart::Comments;
+use IO::Prompt;
 
 my %options;
-GetOptions( \%options, "filename=s" );
+GetOptions( \%options, "filename=s", "minv=s", "maxv=s", "features=s",
+    "allversions!", "allfeatures!" );
 $options{filename} || confess "Need filename!";
+
+my $VersionFilterProvided = 1
+  if ( defined( $options{minv} )
+    or defined( $options{maxv} )
+    or $options{allversions} );
+my $FeatureFilterProvided = 1
+  if ( defined( $options{features} ) or $options{allfeatures} );
 
 #============
 my @SequencesOfInterest;
@@ -24,12 +33,6 @@ my @FilteredData;
 my %FilteredData;
 my %FeatureDistribution;
 my @Filters;
-
-ReadSequencesInFile( $options{filename} );
-ReadAllData();
-### %FeatureDistribution: %FeatureDistribution
-## %FilteredData: %FilteredData
-PrintFilteredData();
 
 sub TrimSequence {
     my ($sequence_string) = @_;
@@ -69,17 +72,17 @@ sub ReadAllData {
 
 sub SetFeatureDistribution {
     %FeatureDistribution = ();
-    %FilteredData = ();
+    %FilteredData        = ();
     for (@FilteredData) {
-        my $terms = NormalizeTestSequence($_->get_terms);
-        my $version = $_->get_version();
-        my $features = NormalizeFeatures($_->get_features());
-        my @results = map { Storable::thaw($_) } @{$_->get_results};
-        my @steps = map { $_->get_steps() } @results;
+        my $terms    = NormalizeTestSequence( $_->get_terms );
+        my $version  = $_->get_version();
+        my $features = NormalizeFeatures( $_->get_features() );
+        my @results  = map { Storable::thaw($_) } @{ $_->get_results };
+        my @steps    = map { $_->get_steps() } @results;
 
         $FeatureDistribution{version}{$version}++;
         $FeatureDistribution{features}{$features}++;
-        push(@{$FilteredData{$terms}{steps}}, @steps);
+        push( @{ $FilteredData{$terms}{steps} }, @steps );
     }
 }
 
@@ -91,34 +94,37 @@ sub NewFilter {
 
 sub ApplyFilter {
     my ($filter) = @_;
-    my ($name, @options) = @$filter;
-    if ($name eq 'version') {
-        my ($min, $max) = @options;
-        FilterVersion($min, $max);
-    } elsif ($name eq 'features') {
+    my ( $name, @options ) = @$filter;
+    if ( $name eq 'version' ) {
+        my ( $min, $max ) = @options;
+        FilterVersion( $min, $max );
+    }
+    elsif ( $name eq 'features' ) {
         my ($features) = @options;
         FilterFeatures($features);
-    } else {
+    }
+    else {
         die "Unknown filter $name";
     }
+    SetFeatureDistribution();
 }
 
 sub FilterFeatures {
     my ($features) = @_;
-    @FilteredData = grep {
-        NormalizeFeatures($_->get_features()) eq $features
-    } @FilteredData;
+    @FilteredData =
+      grep { NormalizeFeatures( $_->get_features() ) eq $features }
+      @FilteredData;
 }
 
 sub FilterVersion {
-    my ($min, $max) = @_;
-    my ($minv, $minr) = split(':', $min);
-    my ($maxv, $maxr) = split(':', $max);
+    my ( $min,  $max )  = @_;
+    my ( $minv, $minr ) = split( ':', $min );
+    my ( $maxv, $maxr ) = split( ':', $max );
 
     @FilteredData = grep {
-        my ($v, $r) = split(':', $_->get_version());
-        ($minv < $v or ($minv == $v and $minr <= $r)) and
-        ($maxv > $v or ($maxv == $v and $maxr >= $r));
+        my ( $v, $r ) = split( ':', $_->get_version() );
+        ( $minv < $v or ( $minv == $v and $minr <= $r ) )
+          and ( $maxv > $v or ( $maxv == $v and $maxr >= $r ) );
     } @FilteredData;
 }
 
@@ -129,12 +135,25 @@ sub UndoLastFilter {
         ApplyFilter($_);
     }
 }
-    
 
-sub NormalizeFeatures {
-    my ($features_string) = @_;
-    # TODO: dummy fn...
-    return $features_string;
+{
+    my @Inconsequential_features = qw{debug CodeletTree
+      LogActivations debugMAX};
+    my %Inconsequential_features =
+      map { ( "-f=$_" => 1 ) } @Inconsequential_features;
+
+    ### Inconsequential_features: %Inconsequential_features
+
+    sub NormalizeFeatures {
+        my ($features_string) = @_;
+        return '' unless $features_string;
+
+        my @parts =
+          grep { !$Inconsequential_features{$_} }
+          split( ';', $features_string );
+        return '' unless @parts;
+        return join( ';', @parts );
+    }
 }
 
 sub PrintFilteredData {
@@ -144,12 +163,86 @@ sub PrintFilteredData {
         PrintStats($steps_ref);
     }
 }
-  
+
 sub PrintStats {
     my ($array_ref) = @_;
     print "\tCount:\t", scalar(@$array_ref), "\n";
     my $vector = vector($array_ref);
-    print "\tMean:\t", mean($array_ref), "\n";
+    print "\tMean:\t",   mean($array_ref),   "\n";
     print "\tStddev:\t", stddev($array_ref), "\n";
 }
+
+sub DecideFiltersAndApply {
+    DecideVersionFilterAndApply();
+    DecideFeatureFilterAndApply();
+}
+
+sub DecideVersionFilterAndApply {
+    if ($VersionFilterProvided) {
+        return if $options{allversions};
+        my $minv = $options{minv} // "0:0";
+        my $maxv = $options{maxv} // "100000: 100000";
+        $maxv = "$maxv:10000" unless $maxv =~ /:/;
+        NewFilter( [ 'version', $minv, $maxv ] );
+        return;
+    }
+    else {
+        InteractiveVersionFilterAndApply();
+    }
+}
+
+sub DecideFeatureFilterAndApply {
+    if ($FeatureFilterProvided) {
+        return if $options{allfeatures};
+        NewFilter( [ 'features', $options{features} ] );
+        return;
+    }
+    else {
+        InteractiveFeaturesFilterAndApply();
+    }
+}
+
+sub InteractiveVersionFilterAndApply {
+    my @versions = keys( %{ $FeatureDistribution{version} } );
+    return if @versions == 1;
+    die "No versions left!" unless @versions;
+
+    @versions = sort { CompareVersions( $a, $b ) } @versions;
+    say "Data from multiple versions of Seqsee available:\n",
+      join( ', ', @versions );
+
+    say "You can narrow this down. ";
+    my $minv = prompt( 'minimum version: ', -m => \@versions );
+    my $maxv = prompt( 'maximum version: ', -m => \@versions );
+    NewFilter( [ 'version', $minv, $maxv ] );
+    return;
+}
+
+sub InteractiveFeaturesFilterAndApply {
+    my @features = keys( %{ $FeatureDistribution{features} } );
+    return if @features <= 1;
+    say
+      "Data from runs of Seqsee with different features turned onavailable:\n",
+      join( ', ', @features );
+    say "You can narrow this down. ";
+    my $feature_set =
+      prompt( "Choose feature sets: ", -m => [ @features, 'ALL' ] );
+    NewFilter( [ 'features', $feature_set ] ) unless $feature_set eq 'ALL';
+    return;
+}
+
+sub CompareVersions {
+    my ( $v1, $v2 ) = @_;
+    my @v1 = split( ':', $v1 );
+    my @v2 = split( ':', $v2 );
+    return ( $v1[0] <=> $v2[0] ) || ( $v1[1] <=> $v2[1] );
+}
+
+ReadSequencesInFile( $options{filename} );
+ReadAllData();
+
+DecideFiltersAndApply();
+### %FeatureDistribution: %FeatureDistribution
+## %FilteredData: %FilteredData
+PrintFilteredData();
 
