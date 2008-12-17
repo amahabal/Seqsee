c385e1ef354d9d3f90e3c9249cddd897574cfd79
amahabal <amahabal@19b06628-3642-0410-8fdd-33ae7300e995>
2008-12-17 05:37:45 +0000

    Started adding documentation to the code.
    git-svn-id: http://seqsee1.googlecode.com/svn/trunk@1069 19b06628-3642-0410-8fdd-33ae7300e995

--- lib/SStream2.pm
+++ lib/SStream2.pm
@@ -259,3 +259,78 @@ sub _is_there_a_hit {
 }
 
 1;
+
+
+__END__
+
+=head1 NAME
+
+SStream2 - A class for managing streams of thought
+           
+=head1 VERSION
+
+This document describes SStream2 for revision $Revision$
+
+=head1 SYNOPSIS
+  
+=head1 DESCRIPTION
+
+Each instance of this class manages a single stream of thought. Seqsee
+currently only ever uses a single stream, but subsequent extensions
+are likely to require multiple streams. The single stream that Seqsee
+has is stored in the variable C< $Global::MainStream >.
+
+=head2 The purpose of a stream
+
+This is best described in chapter 5 of the dissertation, which deals with context. The last few thoughts (currently 10, as stored in the attribute C<MaxOlderThoughts>) form the context in which subsequent thoughts are understood.
+
+Ideas relevant to this discussion include the notions of the fringe and the action fringe. A far more detailed description of these ideas can again be found in chapter 5.
+
+=head2 How it works
+
+Each entry in the stream satisfies the interface C<Thought>.
+
+=head1 INTERFACE 
+
+=head1 DIAGNOSTICS
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+SStream2 requires no configuration files or environment variables.
+
+=head1 DEPENDENCIES
+
+None.
+
+=head1 INCOMPATIBILITIES
+
+None reported.
+
+=head1 BUGS AND LIMITATIONS
+
+=head1 AUTHOR
+
+Abhijit Mahabal  C<< amahabal@gmail.com >>
+
+=head1 DISCLAIMER OF WARRANTY
+
+BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
+FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
+OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
+YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+NECESSARY SERVICING, REPAIR, OR CORRECTION.
+
+IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
+REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
+LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
+OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
+THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
+RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
+FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
+SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
