1c9b9ff4be1a8e6bc657510e25e685b067999550
amahabal <amahabal@19b06628-3642-0410-8fdd-33ae7300e995>
2009-01-04 06:48:43 +0000

    removed files now superceded by Perf/
    git-svn-id: http://seqsee1.googlecode.com/svn/trunk@1095 19b06628-3642-0410-8fdd-33ae7300e995

--- performance/AllGraphBuilder.pl
+++
@@ -1,6 +0,0 @@
-for my $filename (<performance/GraphSpecs/*>) {
-    my $filename_copy = $filename;
-    $filename_copy =~ s#GraphSpecs#generated_images#;
-    $filename_copy .= '.eps';
-    system "perl performance/PerfBarChart.pl --graph_spec=$filename --outfile=$filename_copy";
-}
