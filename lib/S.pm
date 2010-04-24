8c7decf2390cd99fa84821992017366c162ee29d
Abhijit Mahabal <amahabal@gmail.com>
2010-04-24 11:49:50 -0700

    Tidy

--- lib/S.pm
+++ lib/S.pm
@@ -77,11 +77,11 @@ our $ODD      = $SCat::Odd::Odd;
 our $EVEN     = $SCat::Even::Even;
 
 our $DOUBLE = SMetonymType->new(
-    {
-        category  => $S::SAMENESS,
-        name      => "each",
-        info_loss => { length => 2 },
-    }
+  {
+    category  => $S::SAMENESS,
+    name      => "each",
+    info_loss => { length => 2 },
+  }
 );
 
 package DIR;
@@ -89,8 +89,10 @@ use strict;
 use warnings;
 use Carp;
 
-use overload '~~' => 'literal_comparison_hack_for_smart_match',
-  fallback => 1;
+use overload
+'~~'     => 'literal_comparison_hack_for_smart_match',
+fallback => 1;
+
 sub literal_comparison_hack_for_smart_match {
   return $_[0] eq $_[1];
 }
@@ -106,25 +108,25 @@ sub UNKNOWN { $UNKNOWN }
 sub NEITHER { $NEITHER }
 
 sub Flip {
-    my ($self) = @_;
-    return $LEFT  if $self eq $RIGHT;
-    return $RIGHT if $self eq $LEFT;
-    confess "Flip called on weird direction";
+  my ($self) = @_;
+  return $LEFT  if $self eq $RIGHT;
+  return $RIGHT if $self eq $LEFT;
+  confess "Flip called on weird direction";
 }
 
 sub PotentiallyExtendible {
-    my ($self) = @_;
-    return ( $self eq $LEFT or $self eq $RIGHT );
+  my ($self) = @_;
+  return ( $self eq $LEFT or $self eq $RIGHT );
 }
 
 sub IsLeftOrRight {
-    my ($self) = @_;
-    return ( $self eq $LEFT or $self eq $RIGHT );
+  my ($self) = @_;
+  return ( $self eq $LEFT or $self eq $RIGHT );
 }
 
 sub as_text {
-    my ($self) = @_;
-    return $self->{text};
+  my ($self) = @_;
+  return $self->{text};
 }
 
 package POS_MODE;
@@ -135,21 +137,21 @@ sub FORWARD  { $FORWARD }
 sub BACKWARD { $BACKWARD }
 
 sub as_text {
-    my ($self) = @_;
-    return $self->{mode};
+  my ($self) = @_;
+  return $self->{mode};
 }
 
 sub get_memory_dependencies { return; }
 
 sub serialize {
-    my ($self) = @_;
-    return $self->{mode};
+  my ($self) = @_;
+  return $self->{mode};
 }
 
 sub deserialize {
-    my ( $package, $str ) = @_;
-    no strict 'refs';
-    return ${$str};
+  my ( $package, $str ) = @_;
+  no strict 'refs';
+  return ${$str};
 }
 
 package METO_MODE;
@@ -157,12 +159,13 @@ use strict;
 use Carp;
 use warnings;
 
-use overload '~~' => 'literal_comparison_hack_for_smart_match',
-  fallback => 1;
+use overload
+'~~'     => 'literal_comparison_hack_for_smart_match',
+fallback => 1;
+
 sub literal_comparison_hack_for_smart_match {
   return $_[0] eq $_[1];
 }
-
 
 our $NONE      = bless { mode => 'NONE' },      'METO_MODE';
 our $SINGLE    = bless { mode => 'SINGLE' },    'METO_MODE';
@@ -176,47 +179,47 @@ sub ALL       { $ALL }
 sub OTHER     { $OTHER }
 
 sub as_text {
-    my ($self) = @_;
-    return $self->{mode};
+  my ($self) = @_;
+  return $self->{mode};
 }
 
 sub is_position_relevant {
-    my ($self) = @_;
-    if ( $self eq $SINGLE or $self eq $ALLBUTONE ) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
+  my ($self) = @_;
+  if ( $self eq $SINGLE or $self eq $ALLBUTONE ) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
 }
 
 sub is_metonymy_present {
-    my ($self) = @_;
-    return ( $self eq $NONE ) ? 0 : 1;
+  my ($self) = @_;
+  return ( $self eq $NONE ) ? 0 :1;
 }
 
 sub get_memory_dependencies { return; }
 
 sub serialize {
-    my ($self) = @_;
-    return $self->{mode};
+  my ($self) = @_;
+  return $self->{mode};
 }
 
 sub deserialize {
-    my ( $package, $str ) = @_;
-    no strict 'refs';
-    given ($str) {
-        when ('NONE')      { return $NONE }
-        when ('SINGLE')    { return $SINGLE }
-        when ('ALLBUTONE') { return $ALLBUTONE }
-        when ('ALL')       { return $ALL }
-        when ('OTHER')     { return $OTHER }
-        confess "Unknown!";
-    }
+  my ( $package, $str ) = @_;
+  no strict 'refs';
+  given ($str) {
+    when ('NONE')      { return $NONE }
+    when ('SINGLE')    { return $SINGLE }
+    when ('ALLBUTONE') { return $ALLBUTONE }
+    when ('ALL')       { return $ALL }
+    when ('OTHER')     { return $OTHER }
+    confess "Unknown!";
+  }
 }
 
 sub get_pure {
-    return $_[0];
+  return $_[0];
 }
 
 package EXTENDIBILE;
@@ -233,11 +236,11 @@ sub PERHAPS { $PERHAPS }
 sub UNKNOWN { $UNKNOWN }
 
 use overload (
-    q{bool} => sub {
-        my ($self) = @_;
-        return ( $self->{mode} eq 'NO' ) ? 0 : 1;
-    },
-    fallback => 1,
+  q{bool} => sub {
+    my ($self) = @_;
+    return ( $self->{mode} eq 'NO' ) ? 0 :1;
+  },
+  fallback => 1,
 );
 
 package RELN_SCHEME;
@@ -259,17 +262,17 @@ sub ELEMENT { $ELEMENT }
 
 # Can/should be influenced by activations.
 sub PickOne {
-    if ( SUtil::toss(0.25) ) {
-        return $GROUP;
-    }
-    else {
-        return $ELEMENT;
-    }
+  if ( SUtil::toss(0.25) ) {
+    return $GROUP;
+  }
+  else {
+    return $ELEMENT;
+  }
 }
 
 sub IsUnitGroups {
-    my ($mode) = @_;
-    return ( $mode eq $GROUP ) ? 1 : 0;
+  my ($mode) = @_;
+  return ( $mode eq $GROUP ) ? 1 :0;
 }
 
 package DISTANCE;
@@ -277,36 +280,36 @@ use strict;
 use warnings;
 
 sub InElements {
-    my ($distance) = @_;
-    bless [ $distance, $DISTANCE_MODE::ELEMENT ], 'DISTANCE';
+  my ($distance) = @_;
+  bless [ $distance, $DISTANCE_MODE::ELEMENT ], 'DISTANCE';
 }
 
 sub InGroups {
-    my ($distance) = @_;
-    bless [ $distance, $DISTANCE_MODE::GROUP ], 'DISTANCE';
+  my ($distance) = @_;
+  bless [ $distance, $DISTANCE_MODE::GROUP ], 'DISTANCE';
 }
 
 sub Zero {
-    bless [ 0, $DISTANCE_MODE::GROUP ], 'DISTANCE';
+  bless [ 0, $DISTANCE_MODE::GROUP ], 'DISTANCE';
 }
 
 sub IsNonZero {
-    return $_[0]->[0];
+  return $_[0]->[0];
 }
 
 sub IsUnitGroups {
-    my ($self) = @_;
-    return $self->[1]->IsUnitGroups();
+  my ($self) = @_;
+  return $self->[1]->IsUnitGroups();
 }
 
 sub GetMagnitude {
-    my ($self) = @_;
-    return $self->[0];
+  my ($self) = @_;
+  return $self->[0];
 }
 
 sub as_text {
-    my ($self) = @_;
-    return "$self->[0] " . $self->[1]->{mode};
+  my ($self) = @_;
+  return "$self->[0] " . $self->[1]->{mode};
 }
 
 1;
